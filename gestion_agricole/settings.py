d862ed651de114d5a8af9e831d6eb37ed9112e78
Fousseyni1211 <fousseynimoussaberthe@gmail.com>
2026-03-12 01:14:56 +0000

    add openpyxl

--- gestion_agricole/settings.py
+++ gestion_agricole/settings.py
@@ -123,7 +123,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 # STATIC
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / "agricole" / "static",
